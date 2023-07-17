33f3c659e27217b4d0a950dd37ff47898469eebc
DanyloYamelynets <130572960+DanyloYamelynets@users.noreply.github.com>
2023-07-17 16:22:29 +0300

    task done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,50 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Buttons from './Buttons/Buttons';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-export class App extends Component {
-  state = {
+export default function App() {
+  const [states, setStates] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-  };
+  });
 
-  feedbackBtnClick = e => {
+  const feedbackBtnClick = e => {
     const feedbackName = e.target.name;
-    this.setState(prevState => {
+    setStates(prevState => {
       return {
         [feedbackName]: prevState[feedbackName] + 1,
       };
     });
   };
-  countTotalFeedback = () => {
-    const total = this.state.good + this.state.neutral + this.state.bad;
+  const countTotalFeedback = () => {
+    const total = states.good + states.neutral + states.bad;
     return total;
   };
-  countPositiveFeedbackPercentage = () => {
-    const good = this.state.good;
-    const totalFeedback = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const good = states.good;
+    const totalFeedback = countTotalFeedback();
     return totalFeedback > 0 ? (good * 100) / totalFeedback : 0;
   };
-  render() {
-    const options = Object.keys(this.state);
-    const totalFeedback = this.countTotalFeedback();
-    return (
-      <div className="container">
-        <Buttons feedbackBtnClick={this.feedbackBtnClick} options={options} />
 
-        {totalFeedback > 0 ? (
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            countTotalFeedback={this.countTotalFeedback}
-            countPositiveFeedbackPercentage={
-              this.countPositiveFeedbackPercentage
-            }
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </div>
-    );
-  }
+  const options = Object.keys(states);
+  const totalFeedback = countTotalFeedback();
+  return (
+    <div className="container">
+      <Buttons feedbackBtnClick={feedbackBtnClick} options={options} />
+
+      {totalFeedback > 0 ? (
+        <Statistics
+          good={states.good}
+          neutral={states.neutral}
+          bad={states.bad}
+          countTotalFeedback={countTotalFeedback}
+          countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
+        />
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </div>
+  );
 }
